c3f54d7aa825c8e9454e30ab7e9fdb19e40adeb0
nnatividad <k4lani@gmail.com>
2024-05-27 11:11:40 -0700

    navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,13 @@
+import "./NavBar.css";
+
+export default function NavBar() {
+    return (
+      <nav className = "nav-bar">
+        <img src='/BeltoLogo.svg' alt="Logo" className="logo" />
+        <div>
+          <ul>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
